a0b8193c93e4c7d9c0abf31fe93ab5fdd7bb005c
elielizondo21 <elielizondo21@hotmail.com>
2016-08-25 17:20:34 -0500

    Initial Load

---
+++ SPContentSearchExtendedWP/SPContentSearchExtendedWP/ContentSearchExtendedWP/ContentSearchExtendedWP.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using Microsoft.Office.Server.Search;
+using Microsoft.Office.Server.Search.WebControls;
+using Microsoft.Office.Server.Audience;
+using Microsoft.SharePoint.Administration;
+
+namespace SPContentSearchExtendedWP.ContentSearchExtendedWP
+{
+    [ToolboxItemAttribute(false)]
+    public class ContentSearchExtendedWP : ContentBySearchWebPart
+    {
+        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+        public ContentSearchExtendedWP()
+        {
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if (this.AppManager != null)
+            {
+                if (this.AppManager.QueryGroups.ContainsKey(this.QueryGroupName) &&
+                 this.AppManager.QueryGroups[this.QueryGroupName].DataProvider != null)
+                {
+                    this.AppManager.QueryGroups[this.QueryGroupName].DataProvider.BeforeSerializeToClient += new BeforeSerializeToClientEventHandler(UpdateQueryText);
+                }
+            }
+            base.OnLoad(e);
+        }
+
+        private void UpdateQueryText(object sender, BeforeSerializeToClientEventArgs e)
+        {
+            try
+            {
+                DataProviderScriptWebPart dataProvider = sender as DataProviderScriptWebPart;
+
+                string currentQueryText = dataProvider.QueryTemplate;
+                string token = "TargetAudienceQuery";
+                if (currentQueryText.Contains(token))
+                {
+                    dataProvider.QueryTemplate = currentQueryText.Replace(token, BuildTAQuery());
+                }
+            }
+            catch (Exception ex)
+            {
+                SPDiagnosticsService.Local.WriteTrace(0, new SPDiagnosticsCategory("Content Search Ext Web Part", TraceSeverity.Medium, EventSeverity.Error),
+                    TraceSeverity.Medium, "UpdateQueryText Failed: " + ex.Message, null);
+            }
+        }
+
+        private string BuildTAQuery()
+        {
+            string query = string.Empty;
+            string audienceGroups = string.Empty;
+            try
+            {
+                SPSite site = new SPSite(SPContext.Current.Web.Url);
+                SPServiceContext context = SPServiceContext.GetContext(site);
+
+                SPUser currentUser = SPContext.Current.Web.CurrentUser;
+                string loginName = currentUser.ToString().Split('|')[1];
+
+                AudienceManager audMgr = new AudienceManager(context);
+                AudienceCollection audiences = audMgr.Audiences;
+
+                for (int i = 0; i < audiences.Count; i++)
+                {
+                    if (audiences[i].AudienceName != "All site users")
+                    {
+                        if (audiences[i].IsMember(loginName))
+                        {
+                            //audienceGroups = audienceGroups + "," + audiences[i].AudienceID; 
+                            //TargetAudience is the managed property name in the search schema
+                            query += "TargetAudience:\"" + audiences[i].AudienceID + "\" OR ";
+                        }
+                    }
+                }
+                query = query.Substring(0, query.LastIndexOf(" OR "));
+
+            }
+            catch (Exception ex)
+            {
+                SPDiagnosticsService.Local.WriteTrace(0, new SPDiagnosticsCategory("Content Search Ext Web Part", TraceSeverity.Medium, EventSeverity.Error),
+                    TraceSeverity.Medium, "Audience Build Failed: " + ex.Message, null);
+            }
+            return query;
+        }
+    }
+}
---
+++ SPContentSearchExtendedWP/SPContentSearchExtendedWP/Features/ContentSearchExtWPFeature/ContentSearchExtWPFeature.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="26479233-3769-4289-91c4-ff2624d00984" description="Allows for Audience Targeting" featureId="26479233-3769-4289-91c4-ff2624d00984" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="SharePoint Content Search Extended Web Part" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="6afbe24b-3820-450d-9280-57b444045e0b" />
+  </projectItems>
+</feature>
